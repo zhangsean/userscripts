41636d66fc3cdf8581f434f034cf53fc24fea90b
Sean Zhang <zxf2342@qq.com>
2021-09-24 22:51:51 +0800

    增加更新页面

--- kinhdown-auto-refresh.user.js
+++ kinhdown-auto-refresh.user.js
@@ -2,11 +2,13 @@
 // @name         KinhDown Auto Refresh
 // @namespace    https://github.com/zhangsean/userscripts/
 // @version      0.4.3
-// @description  Auto refresh page to get valid download link.
+// @description  Auto refresh KinhDown page to get a valid download link.
 // @author       zhangsean
 // @match        https://baidu.kinh.cc/*
-// @match        http://pan.akcsss.cn/*
 // @match        https://kinhbaiducloudweb.zerun.asia/*
+// @updateURL    https://github.com/zhangsean/userscripts/raw/master/kinhdown-auto-refresh.user.js
+// @downloadURL  https://github.com/zhangsean/userscripts/raw/master/kinhdown-auto-refresh.user.js
+// @supportURL   https://github.com/zhangsean/userscripts/issues
 // @icon         https://api.kinh.cc/HtmlStatic/Kinh-Logo.ico
 // @grant        none
 // ==/UserScript==
